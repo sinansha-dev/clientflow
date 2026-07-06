965a2213314bf6b2a204da3a771499c978b3fcda
sinansha-dev <sinumohammed2039@gmail.com>
2026-07-06 08:23:32 +0530

    Pass database URL directly to Prisma

--- apps/api/src/config/prisma.ts
+++ apps/api/src/config/prisma.ts
@@ -1,6 +1,11 @@
-import './env';
 import { PrismaClient } from '@prisma/client';
+import { env } from './env';
 
 export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: env.DATABASE_URL,
+    },
+  },
   log: ['warn', 'error'],
 });
